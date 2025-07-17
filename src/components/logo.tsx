89bbc0050df1b29807f2b030c59b81639c884874
Subodh Rana <subodhrana390@gmail.com>
2025-07-17 16:56:35 +0000

    when click on cropsense ai redirect to / change made in home as well as

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,20 @@
 
 import { Leaf } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div
-      className={cn(
-        'flex items-center gap-2 text-3xl font-bold text-primary font-headline',
-        className
-      )}
-    >
-      <Leaf className="h-8 w-8" />
-      <span>CropSense AI</span>
-    </div>
+    <Link href="/" className="inline-block">
+        <div
+        className={cn(
+            'flex items-center gap-2 text-3xl font-bold text-primary font-headline',
+            className
+        )}
+        >
+        <Leaf className="h-8 w-8" />
+        <span>CropSense AI</span>
+        </div>
+    </Link>
   );
 }
